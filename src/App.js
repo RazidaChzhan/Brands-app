8b75cb03f33a292c2d088e2b722aa26f38984aaa
RazidaChzhan <chrazida@gmail.com>
2019-07-30 06:52:50 +0600

    Подключен второй файл, просмотр деталей реализован

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BrandList from "./components/brandList";
+import ItemList from './components/itemList'
+import BrandDetail from "./components/brandDetail";
+import MainMenu from "./pages/MainMenu";
+import NotFound from "./pages/NotFound";
+import ItemDetail from "./components/itemDetail";
+
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                  <Route exact path="/" component={MainMenu} />
+                  <Route path="/brands/:brand" component={BrandDetail} />
+                  <Route exact path="/brands" component={BrandList} />       
+                  <Route exact path="/products" component={ItemList}/>
+                  <Route path="/products/:product" component={ItemDetail}/>
+                  <Route component={NotFound}/>
+                </Switch>
+            </Router>
+        
+
+        );
+    }
+}           
+                     
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import BrandList from "./brandList";
-import BrandDetail from "./brandDetail";
-import { NavLink } from "react-router-dom";
+import Menu from './App';
 
-import {Helmet} from "react-helmet";
-
-const items = require("./data/brands.json");
-const details = require("./data/items.json");
-
-// const Home = () => (
-//   <div>
-//   <Helmet>
-//     <meta charSet="utf-8" />
-//     <title>Home</title>
-//    </Helmet>
-//     <h2>BrandDetail</h2>
-//     <BrandList/>
-//   </div>
-// )
-
-ReactDOM.render(
-   
-  
-  <BrowserRouter>
-    <Switch>
-      {items.map(item => (
-        <Route
-          path={`/detail/${item.slug}`}
-          component={props => <BrandDetail brand={item} />}
-        />
-      ))}
-      <Route exact path="/" component={BrandList} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+ReactDOM.render(<Menu/>,document.getElementById("root")
 );
